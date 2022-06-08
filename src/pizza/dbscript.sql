539ac93f4f7b0a50061b1618fb429f2c1f465800
xABIBIxD <abdelouadoudhedir@gmail.com>
2022-06-08 15:05:47 +0200

    08.06.2022

--- src/pizza/dbscript.sql
+++ src/pizza/dbscript.sql
@@ -6,7 +6,7 @@ create table pizza(
     beschreibung varchar
 );
 
-insert into pizza (name, beschreibung) VALUES ("Salami", "einfach nur Salami");
+insert into pizza (name, beschreibung) VALUES ('Salami', "einfach nur Salami");
 insert into pizza (name, beschreibung) VALUES ("Spezial", "Mit Pilzen, Käse, Tomaten, Zwiebel und Steak ");
 insert into pizza (name, beschreibung) VALUES ("Peperoni", "Mit Peperoni, Pilzen, Käse, Tomaten, Zwiebel und Steak ");
 
